760a7278967784cfe91961383019b6253b738ea0
Kumatetsu <root@base>
2017-04-19 21:02:00 +0200

    connexion des users

--- server/functions/accept_client.c
+++ server/functions/accept_client.c
@@ -5,6 +5,34 @@
 ** Login   <billau_j@etna-alternance.net>
 ** 
 ** Started on  Mon Apr 17 11:56:40 2017 BILLAUD Jean
-** Last update Mon Apr 17 11:56:42 2017 BILLAUD Jean
+** Last update Wed Apr 19 16:52:59 2017 BILLAUD Jean
 */
 
+#include 	<stdlib.h>
+#include 	<stdio.h>
+#include	<string.h>
+#include 	<unistd.h>
+#include 	<netdb.h>
+#include	<netinet/in.h>
+#include 	<sys/socket.h>
+#include 	<arpa/inet.h>
+#include	<sys/select.h>
+#include	<sys/types.h>
+#include        "../headers/structures.h"
+#include        "../headers/server.h"
+
+void			accept_cli(int listener, t_env *env)
+{
+  struct sockaddr_in	cli_addr;
+  int			client;
+  socklen_t		socklen;
+  t_user		*user;
+  char			*welcome;
+
+  socklen = sizeof(cli_addr);
+  welcome = "Welcome to my_slack\nGive your name:\n"; 
+  client = accept(listener, (struct sockaddr *)&cli_addr, &socklen);
+  user = create_user("", client);
+  add_user_to_chan(env->last, user);
+  send(client, welcome, strlen(welcome), MSG_DONTWAIT);
+}
--- server/functions/user_name.c
+++ server/functions/user_name.c
@@ -5,7 +5,7 @@
 ** Login   <billau_j@etna-alternance.net>
 ** 
 ** Started on  Tue Apr 18 21:25:16 2017 BILLAUD Jean
-** Last update Tue Apr 18 22:12:02 2017 BILLAUD Jean
+** Last update Wed Apr 19 20:50:53 2017 BILLAUD Jean
 */
 
 #include 	<stdlib.h>
@@ -21,37 +21,45 @@
 #include        "../headers/structures.h"
 #include        "../headers/server.h"
 
-void		ask_user_name(t_channel *chan)
+void			ask_user_name(t_env *env)
 {
-  fd_set	readfs;
-  int		nread;
-  char		buffer[20];
-  t_user	*tmp;
-  int		i;
+  fd_set		readfs;
+  int			nread;
+  char			buffer[140];
+  t_user		*tmp;
+  int			i;
+  struct timeval	tv;
 
   i = 4;
-  tmp = chan->first;
-  FD_ZERO(&readfs);
-  while (tmp != NULL)
+  tv.tv_sec = 5;
+  tv.tv_usec = 0;
+  if (env->last->first != NULL)
     {
-      FD_SET(tmp->cli_addr, &readfs);
-      i += 1;
-      tmp = tmp->next;
-    }
-  FD_SET(0, &readfs);
-  tmp = chan->first;
-  select(i, &readfs, NULL, NULL, NULL);  
-  while (tmp != NULL)
-    {
-      if (FD_ISSET(tmp->cli_addr, &readfs))
+      tmp = env->last->first;
+      FD_ZERO(&readfs);
+      while (tmp != NULL)
 	{
-	  my_putstr("client name....\n");
-	  memset(buffer, 0, 20);
-	  nread = read(tmp->cli_addr, buffer, 20);
-	  buffer[nread] = '\0';
-	  my_putstr(buffer);
-	  FD_CLR(tmp->cli_addr, &readfs);
+	  FD_SET(tmp->cli_addr, &readfs);
+	  i += 1;
+	  tmp = tmp->next;
 	}
-      tmp = tmp->next;
+      FD_SET(0, &readfs);
+      tmp = env->last->first;
+      select(i, &readfs, NULL, NULL, &tv);
+      while (tmp != NULL)
+	{
+	  if (FD_ISSET(tmp->cli_addr, &readfs))
+	    {
+	      my_putstr("client name....\n");
+	      memset(buffer, 0, 140);
+	      nread = read(tmp->cli_addr, buffer, 140);
+	      buffer[nread] = '\0';
+	      free(tmp->login);
+	      tmp->login = my_strdup(buffer);
+	      change_user_channel(env->last, env->first, tmp);
+	      FD_CLR(tmp->cli_addr, &readfs);
+	    }
+	  tmp = tmp->next;
+	}
     }
 }
